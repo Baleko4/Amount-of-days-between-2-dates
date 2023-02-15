964a675bc741fc13d2ad66a598fa01377338745e
Baleko4 <103016932+Baleko4@users.noreply.github.com>
2023-02-14 23:57:38 -0300

    Update bInvest.java

--- bInvest.java
+++ bInvest.java
@@ -1,75 +1,94 @@
 import java.util.*;
-class diffDays{
-    public int mtod(int A){
-        if(A==1){
-            A=0;
+class diffDays{ //This class will give me the value we are looking for.
+    public int mtod(int month){ //Responsible for converting the month in the dates to an equivalent number of days.
+        if(month==1){ //If it's January then we don't need to add extra days.
+            month=0;
         }
-        else if(A==2){
-            A=31;
+        else if(month==2){ // After January you add extra days for each month before.
+            month=31;
         }
-        else if(A <=7){
-            if(A %2==1){
-                A=(A-A/2-1)*31 + 28 + (A/2-1)*30;
+        else if(month <=7){
+            if(month %2==1){
+                month=(month-month/2-1)*31 + 28 + (month/2-1)*30;
             }
             else{
-                A=(A*31)/2 + 28 + (A/2-2)*30; 
+                month=(month*31)/2 + 28 + (month/2-2)*30; 
             }
         }
         else{
-            if(A%2==0){
-                A=((A-7)-(A-7)/2-1)*31 + 212 + ((A-7)/2)*30;
+            if(month%2==0){
+                month=((month-7)-(month-7)/2-1)*31 + 212 + ((month-7)/2)*30;
             }
             else{
-                A=((A-7)*31)/2 + 212 + ((A-7)/2-1)*30; 
+                month=((month-7)*31)/2 + 212 + ((month-7)/2-1)*30; 
             }
         }
-        return A;
+        return month;
     }
-    public int diferenca(String A, String B){
-        int x=0,a1,b1,a2,b2,a3,b3;
-        a3= 1000*Character.getNumericValue(A.charAt(6)) + 100*Character.getNumericValue(A.charAt(7)) 
+    public int diferenca(String A, String B){ //Here we get the main value.
+        int Total=0,monthA,monthB,dayA,dayB,yearA,yearB;
+        // From lines 31 to 43 is just getting the numeric values of the dates from the strings.
+        yearA= 1000*Character.getNumericValue(A.charAt(6)) + 100*Character.getNumericValue(A.charAt(7)) 
         + 10*Character.getNumericValue(A.charAt(8)) + Character.getNumericValue(A.charAt(9));
-        b3= 1000*Character.getNumericValue(B.charAt(6)) + 100*Character.getNumericValue(B.charAt(7)) 
-        + 10*Character.getNumericValue(B.charAt(8)) + Character.getNumericValue(B.charAt(9));
-        int i,j=a3;
-        a2= 10*Character.getNumericValue(A.charAt(0)) + Character.getNumericValue(A.charAt(1));
-        b2= 10*Character.getNumericValue(B.charAt(0)) + Character.getNumericValue(B.charAt(1));
 
-        a1= 10*Character.getNumericValue(A.charAt(3)) + Character.getNumericValue(A.charAt(4));
-        b1= 10*Character.getNumericValue(B.charAt(3)) + Character.getNumericValue(B.charAt(4));
-        a1=mtod(a1);
-        b1=mtod(b1);
-        if(a3%4==0 && a1>2){
-            a1++;
+        yearB= 1000*Character.getNumericValue(B.charAt(6)) + 100*Character.getNumericValue(B.charAt(7)) 
+        + 10*Character.getNumericValue(B.charAt(8)) + Character.getNumericValue(B.charAt(9));
+
+        dayA= 10*Character.getNumericValue(A.charAt(0)) + Character.getNumericValue(A.charAt(1));
+
+        dayB= 10*Character.getNumericValue(B.charAt(0)) + Character.getNumericValue(B.charAt(1));
+
+        monthA= 10*Character.getNumericValue(A.charAt(3)) + Character.getNumericValue(A.charAt(4));
+
+        monthB= 10*Character.getNumericValue(B.charAt(3)) + Character.getNumericValue(B.charAt(4));
+
+        monthA=mtod(monthA);
+        monthB=mtod(monthB);
+        int i,j=yearA;
+        if(yearA%4==0 && monthA>2){ // This takes into account the leap year in the number of days in the month of the date.
+            monthA++;
         }
-        if(b3%4==0 && b1>2){
-            b1++;
+        if(yearB%4==0 && monthB>2){
+            monthB++;
         }
-        x=b2+b1-a2-a1;
-        for(i=0;i<3;i++){
-            if(j%4==0){
-                x++;
-                x=x+(b3-j-1)/4;
-                break;
+        Total=dayB+monthB-dayA-monthA;
+        if(yearB-yearA>=2){
+            for(i=0;i<3;i++){ // Takes into account all the extra leap year days from the beggining year to the final.
+                if(j%4==0){
+                    Total++;
+                    Total=Total+(yearB-j-1)/4;
+                    break;
+                }
+                j=j++;
             }
-            j=j++;
         }
-        if(i==3){
-            x=x+(b3-a3)/4;
+        else{
+            for(i=0;i<=yearB-yearA;i++){ // Takes into account all the extra leap year days from the beggining year to the final.
+                if(j%4==0){
+                    Total++;
+                    Total=Total+(yearB-j-1)/4;
+                    break;
+                }
+                j=j++;
+            }
+        }           
+        if(i==3 || i==yearB-yearA+1){
+            Total=Total+(yearB-yearA)/4;
         }
-        x=x+(b3-a3)*365;
-        return x;
+        Total=Total+(yearB-yearA)*365;
+        return Total;
     }
 }
 public class bInvest {
     public static void main(String args[] ){
         Scanner sc = new Scanner (System.in);
-        String data1,data2;
+        String date1,date2;
         System.out.println("Insert here the first date:");
-        data1=sc.nextLine();
+        date1=sc.nextLine();
         System.out.println("Insert here the second one:");
-        data2=sc.nextLine();
+        date2=sc.nextLine();
         diffDays df = new diffDays();
-        System.out.println(df.diferenca(data1, data2));
+        System.out.println(df.diferenca(date1, date2));
+        sc.close();
     }
 }
